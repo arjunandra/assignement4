97c5b4416eb65c01bd21345f63ae8e9df9d352cf
ameykudari <ameykudari05@gmail.com>
2019-04-04 10:05:47 +0530

    demo commit- move html pages to templates, use url_for to redirect

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
 from flask import Flask, render_template, flash, url_for
 
+app = Flask(__name__)
+# name will be changed later, configs will be added later
+
+@app.route("/")
+def IntroductionPage():
+    return render_template('Introduction.html')
+
+# change links to the route, demo shown below,
+# note: Theory page not modified, only routing is shown here, so all other pages will follow this format
+# add app.route to each page that needs one, use url_for() to redirect do we dont face problems if we move files anytime.
+
+@app.route("/Theory")
+def TheoryPage():
+    return render_template('Theory.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

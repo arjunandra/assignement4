c8dbad333e49f013e57d134cc53e368a3ec3bbd7
Arjun Andra <arjun.andra@gmail.com>
2019-04-04 16:20:09 +0530

    move files to templates and update app.py

--- app.py
+++ app.py
@@ -15,5 +15,34 @@ def IntroductionPage():
 def TheoryPage():
     return render_template('Theory.html')
 
+@app.route("/Objective")
+def ObjectivePage():
+    return render_template('Objective.html')
+
+@app.route("/Experiment")
+def ExperimentPage():
+    return render_template('Experiment.html')
+
+@app.route("/Manual")
+def ManualPage():
+    return render_template('Manual.html')
+
+@app.route("/Quizzes")
+def QuizzesPage():
+    return render_template('Quizzes.html')
+
+@app.route("/Procedure")
+def ProcedurePage():
+    return render_template('Procedure.html')
+
+@app.route("/Refrences")
+def RefrencesPage():
+    return render_template('Refrences.html')
+
+@app.route("/Feedback")
+def FeedbackPage():
+    return render_template('Feedback.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)

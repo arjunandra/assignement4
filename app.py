fed44c901d32edb8a80e6556896d9e3e09b7350c
ameykudari <ameykudari05@gmail.com>
2019-04-14 11:23:08 +0530

    Finish all the backend work

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
-from flask import Flask, render_template, flash, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+global_score=5
 
 app = Flask(__name__)
 # name will be changed later, configs will be added later
+app.secret_key = 'super secret key'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class QuizResults(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.Integer)
+
+    #def __init__(self, score):
+    #    self.score=score;
+
+    def __repr__(self):
+        return f"{self.score}"
 
 @app.route("/")
 def IntroductionPage():
     return render_template('Introduction.html')
-
-# change links to the route, demo shown below,
-# note: Theory page not modified, only routing is shown here, so all other pages will follow this format
-# add app.route to each page that needs one, use url_for() to redirect do we dont face problems if we move files anytime.
 
 @app.route("/Theory")
 def TheoryPage():
@@ -29,7 +43,7 @@ def ManualPage():
 
 @app.route("/Quizzes")
 def QuizzesPage():
-    return render_template('Quizzes.html')
+    return render_template('Quizzes.html', scores=QuizResults.query.all())
 
 @app.route("/Procedure")
 def ProcedurePage():
@@ -43,6 +57,18 @@ def RefrencesPage():
 def FeedbackPage():
     return render_template('Feedback.html')
 
+
+@app.route("/Results")
+def ResultPage():
+    return render_template('Results.html', tscore=QuizResults.query.all()[-1])
+
+@app.route("/add_score/<marks>")
+def addScrore(marks):
+    new = QuizResults(score=marks)
+    db.session.add(new)
+    db.session.commit()
+    #return redirect(url_for(QuizzesPage))
+    return redirect(url_for('ResultPage'))
 
 if __name__ == '__main__':
     app.run(debug=True)
